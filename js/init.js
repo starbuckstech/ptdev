dddbe3d2d27611e43f04799543d73ae635d3d4f1
Adam Starbuck <me.starbuck@gmail.com>
2015-02-19 17:46:35 -0600

    Fixing sidebar nav

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav({menuWidth: 300, activationWidth: 70, closeOnClick: true});
+    $('.button-collapse').sideNav({menuWidth: 240, activationWidth: 90, closeOnClick: true});
     $(".dropdown-button").dropdown( { hover: false, constrain_width: false, });
 
   }); // end of document ready
