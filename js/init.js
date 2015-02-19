f1c472110f676b01328cddd97357950de52010cc
Adam Starbuck <me.starbuck@gmail.com>
2015-02-19 16:59:02 -0600

    Added content. Added file structure.

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $(".dropdown-button").dropdown( { hover: false, constrain_width: false, });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
