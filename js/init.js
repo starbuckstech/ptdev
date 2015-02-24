84681abd2d5cdf205e83f95b89ba75f3579cd833
Adam Starbuck <me.starbuck@gmail.com>
2015-02-23 22:32:10 -0600

    Mainly changed the laser tag form
    It's now located on my domain rather than Google's but it still refer's
    to Google's form service

--- js/init.js
+++ js/init.js
@@ -3,7 +3,8 @@
 
     $('.button-collapse').sideNav({menuWidth: 240, activationWidth: 70, closeOnClick: true});
     $(".dropdown-button").dropdown( { hover: false, constrain_width: false, });
-     $('.datepicker').pickadate();
+    $('.datepicker').pickadate();
+    $('select').material_select();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
